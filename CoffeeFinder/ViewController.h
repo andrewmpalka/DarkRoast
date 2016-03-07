43cbcf11cc26687d466623f1d7a89b798d8b66ed
Andy Palka <andrewmpalka@gmail.com>
2016-03-07 11:58:22 -0600

    Initial Commit

---
+++ CoffeeFinder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoffeeFinder
+//
+//  Created by Andrew Palka on 3/7/16.
+//  Copyright © 2016 Andrew Palka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
